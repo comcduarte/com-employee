0fc70c32c3101dba1cb0fefbc89e0bcbfcb22724
Christopher Duarte <christopher.duarte@middletownct.gov>
2024-03-21 14:07:36 -0400

    Version

--- src/Module.php
+++ src/Module.php
@@ -3,6 +3,9 @@ namespace Employee;
 
 class Module
 {
+    const TITLE = "Employee Module";
+    const VERSION = "v1.0.6";
+    
     public function getConfig()
     {
         return include __DIR__ . '/../config/module.config.php';
